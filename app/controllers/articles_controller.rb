44a1b1fb9f5594b7cf7457a76e9a4368a9f0b84d
Patrick Brown <patrick.arthur.brown@gmail.com>
2018-04-05 14:27:37 -0700

    Order from newest to oldest

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,7 +6,7 @@ class ArticlesController < ApplicationController
   # GET /articles
   # GET /articles.json
   def index
-    @articles = Article.all
+    @articles = Article.all.order(created_at: :desc)
   end
 
   # GET /articles/1
