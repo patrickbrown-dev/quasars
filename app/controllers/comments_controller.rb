7b31a4bea4525013bf86f5c4e12f3d92b61dba9a
Patrick Brown <patrick.arthur.brown@gmail.com>
2018-04-05 20:48:38 -0700

    Fix comment edit back

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,67 +3,47 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!
   before_action :user_consistency_check, only: [:edit, :update, :destroy]
 
-  # GET /comments/1/edit
   def edit
   end
 
-  # POST /comments
-  # POST /comments.json
   def create
     @comment = Comment.new(comment_params)
 
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to @comment.article, notice: 'Comment was successfully created.' }
-        format.json { render :show, status: :created, location: @comment }
-      else
-        format.html { render :new }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
+    if @comment.save
+      redirect_to @comment.article, notice: 'Comment was successfully created.'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /comments/1
-  # PATCH/PUT /comments/1.json
   def update
-    respond_to do |format|
-      if @comment.update(comment_params)
-        format.html { redirect_to @comment.article, notice: 'Comment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @comment }
-      else
-        format.html { render :edit }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
+    if @comment.update(comment_params)
+      redirect_to @comment.article, notice: 'Comment was successfully updated.'
+    else
+      render :edit
     end
   end
 
-  # DELETE /comments/1
-  # DELETE /comments/1.json
   def destroy
     article = @comment.article
     @comment.destroy
 
-    respond_to do |format|
-      format.html { redirect_to article, notice: 'Comment was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to article, notice: 'Comment was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_comment
-      @comment = Comment.find(params[:id])
-    end
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def comment_params
-      params.require(:comment).permit(:article_id, :user_id, :body)
-    end
+  def comment_params
+    params.require(:comment).permit(:article_id, :user_id, :body)
+  end
 
-    def user_consistency_check
-      if current_user != @comment.user
-        flash.alert = "You're not authorized to perform that action."
-        redirect_to @comment.article
-      end
+  def user_consistency_check
+    if current_user != @comment.user
+      flash.alert = "You're not authorized to perform that action."
+      redirect_to @comment.article
     end
+  end
 end
