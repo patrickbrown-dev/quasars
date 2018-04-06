5b1c4c13f41e7a4ee96f2c35484991f90d8e3cf4
Patrick Brown <patrick.arthur.brown@gmail.com>
2018-04-05 20:41:16 -0700

    Paginate articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,83 +3,59 @@ class ArticlesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :user_consistency_check, only: [:edit, :update, :destroy]
 
-  # GET /articles
-  # GET /articles.json
   def index
-    @articles = Article.all.order(created_at: :desc)
+    page = params[:page] || 1
+    @articles = Article.order(created_at: :desc).page(page).per(20)
   end
 
-  # GET /articles/1
-  # GET /articles/1.json
   def show
     @comment = Comment.new(user_id: current_user.try(:id), article_id: @article.id)
   end
 
-  # GET /articles/new
   def new
     @article = Article.new
   end
 
-  # GET /articles/1/edit
   def edit
   end
 
-  # POST /articles
-  # POST /articles.json
   def create
     @article = Article.new(article_params)
     @article.user = current_user
 
-    respond_to do |format|
-      if @article.save
-        format.html { redirect_to @article, notice: 'Article was successfully created.' }
-        format.json { render :show, status: :created, location: @article }
-      else
-        format.html { render :new }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
+    if @article.save
+      redirect_to @article, notice: 'Article was successfully created.'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /articles/1
-  # PATCH/PUT /articles/1.json
   def update
-    respond_to do |format|
-      if @article.update(article_params)
-        format.html { redirect_to @article, notice: 'Article was successfully updated.' }
-        format.json { render :show, status: :ok, location: @article }
-      else
-        format.html { render :edit }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
+    if @article.update(article_params)
+      redirect_to @article, notice: 'Article was successfully updated.'
+    else
+      render :edit
     end
   end
 
-  # DELETE /articles/1
-  # DELETE /articles/1.json
   def destroy
     @article.destroy
-    respond_to do |format|
-      format.html { redirect_to "/", notice: 'Article was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to "/", notice: 'Article was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_article
-      @article = Article.find(params[:id])
-    end
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def article_params
-      params.require(:article).permit(:user_id, :title, :url, :description)
-    end
+  def article_params
+    params.require(:article).permit(:user_id, :title, :url, :description, :page)
+  end
 
-    def user_consistency_check
-      if current_user != @article.user
-        flash.alert = "You're not authorized to perform that action."
-        redirect_to @article
-      end
+  def user_consistency_check
+    if current_user != @article.user
+      flash.alert = "You're not authorized to perform that action."
+      redirect_to @article
     end
+  end
 end
