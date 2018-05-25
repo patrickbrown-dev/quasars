7b7d42345bdddd507ddd6da111abc74c6309957e
Patrick Brown <patrick.arthur.brown@gmail.com>
2018-05-24 19:19:14 -0700

    Be more descriptive when unhealthy

--- app/controllers/healthcheck_controller.rb
+++ app/controllers/healthcheck_controller.rb
@@ -1,16 +1,22 @@
 class HealthcheckController < ApplicationController
   def index
     ActiveRecord::Base.connection
+    render plain: message("healthy"), status: 200
+  rescue => e
+    logger.error(e)
+    render plain: message("unhealthy"), status: 500
+  end
+
+  private
+  def message(health)
     env     = Rails.env
     deploy  = ENV.fetch("DEPLOY") { "local" }
     sha     = `git log -n 1 --format=%h`
     message = <<-MSG.strip_heredoc
-      status:\tok
+      status:\t#{health}
       env:\t#{env}
       deploy:\t#{deploy}
       sha:\t#{sha}
     MSG
-
-    render plain: message, status: 200
   end
 end
