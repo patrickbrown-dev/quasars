afb94abd0bac8cd9aaaf97d442ef5f41d08a649a
Patrick Brown <patrick.arthur.brown@gmail.com>
2018-05-26 23:42:27 -0400

    Allow posts without url if description is provided

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class UrlValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    record.errors[attribute] << (options[:message] || "is not a URL") unless is_url?(value)
+    record.errors[attribute] << (options[:message] || "is not a URL") unless is_url?(value) || value.empty?
   end
 
   private
@@ -19,9 +19,11 @@ class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
 
-  validates :user, :title, :url, presence: true
+  validates :user, :title, presence: true
   validates :url, :uid, uniqueness: true
   validates :url, url: true
+
+  validate :url_and_or_description
 
   before_create :set_uid
 
@@ -36,8 +38,18 @@ class Article < ApplicationRecord
     uri.host
   end
 
+  def description_only?
+    url.empty?
+  end
+
   private
   def set_uid
     self.uid = "#{SecureRandom.hex(3)}-#{title.gsub(/[^0-9a-z ]/i, '').gsub(/\W/, '_').downcase}"
+  end
+
+  def url_and_or_description
+    if url.empty? && description.empty?
+      errors.add(:base, "Url and Description cannot both be empty")
+    end
   end
 end
