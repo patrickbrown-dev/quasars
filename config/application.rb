64e1bb1d62f455b272c09119f2fa85137889a087
Patrick Brown <patrick.arthur.brown@gmail.com>
2018-06-23 19:21:45 -0700

    Add rspec and rubocop

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 module App
   class Application < Rails::Application
     config.generators.javascript_engine = :js
+    config.generators.test_framework = :rspec
     config.load_defaults 5.1
   end
 end
