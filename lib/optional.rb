1921d084eeba683dbef97de085730604c1774e34
Patrick Brown <patrick.arthur.brown@gmail.com>
2018-11-06 17:50:20 -0800

    Remove unneccessary inheritance

--- lib/optional.rb
+++ lib/optional.rb
@@ -1,29 +1,29 @@
 class Optional
+  class Some
+    def initialize(obj)
+      @obj = obj
+    end
+
+    def get
+      @obj
+    end
+
+    def none?
+      false
+    end
+  end
+
+  class None
+    def none?
+      true
+    end
+  end
+
   def self.none
     None.new
   end
 
   def self.some(obj)
     Some.new(obj)
-  end
-end
-
-class Some < Optional
-  def initialize(obj)
-    @obj = obj
-  end
-
-  def get
-    @obj
-  end
-
-  def none?
-    false
-  end
-end
-
-class None < Optional
-  def none?
-    true
   end
 end
