19a6873a3e8982188fdaf716fb6441ffa489f201
Patrick Brown <patrick.arthur.brown@gmail.com>
2018-11-06 17:16:04 -0800

    Add mailer specs

--- spec/mailers/comment_mailer_spec.rb
+++ spec/mailers/comment_mailer_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe CommentMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:mail) do
+    CommentMailer.with(comment: comment, user: user).comment_email
+  end
+
+  let(:comment) { FactoryBot.build(:comment) }
+  let(:user) { FactoryBot.build(:user) }
+
+  it 'sets the subject' do
+    expect(mail.subject)
+      .to eq("#{comment.user.username} replied to your post on quasars")
+  end
+
+  it 'sents the destination address' do
+    expect(mail.to).to eq([user.email])
+  end
+
+  it 'sents the from address' do
+    expect(mail.from).to eq(['notifications@quasa.rs'])
+  end
+
+  it 'renders the body' do
+    expect(mail.body.encoded)
+      .to match("#{comment.user.username} replied to your post on quasars")
+  end
+
+  it 'includes the body of the comment' do
+    expect(mail.body.encoded).to match(comment.body)
+  end
+
+  it 'includes a link to the article' do
+    expect(mail.body.encoded)
+      .to match(article_url(comment.article.uid))
+  end
 end
