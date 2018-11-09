a5ab8ec30e2849c4c8043b91860babaa66d2f356
Patrick Brown <patrick.arthur.brown@gmail.com>
2018-11-09 14:00:31 -0800

    Set hotness to created_at desc for now

--- app/models/concerns/voteable.rb
+++ app/models/concerns/voteable.rb
@@ -4,7 +4,10 @@ module Voteable
   included do
     has_many :votes, as: :voteable, dependent: :destroy
 
-    scope :hot, -> { order(karma: :desc).order(created_at: :desc) }
+    # TODO: This is a wildly insufficient algorithm to capture
+    # the "hotness" of a given article, but created_at :desc is
+    # the best user experience for now.
+    scope :hot, -> { order(created_at: :desc) }
   end
 
   def upvoted_by_user?(user)
