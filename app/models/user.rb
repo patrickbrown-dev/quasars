56eb44e1775ac75f0732058a6ad4a4fdf704705b
Patrick Brown <patrick.arthur.brown@gmail.com>
2018-11-14 21:00:43 -0800

    Include comments in user karma

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,9 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
 
   def karma
-    articles.map(&:karma).reduce(:+) || 0
+    total = 0
+    total += articles.map(&:karma).reduce(:+) || 0
+    total += comments.map(&:karma).reduce(:+) || 0
+    total
   end
 end
