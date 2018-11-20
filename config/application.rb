ed6d0beba30c0cbd1fb900f2c46af797a09df19c
Patrick Brown <patrick.arthur.brown@gmail.com>
2018-11-20 11:13:14 -0800

    fixup! Appease the almighty rubocop

--- config/application.rb
+++ config/application.rb
@@ -4,8 +4,10 @@ require 'rails/all'
 
 Bundler.require(*Rails.groups)
 
-class App::Application < Rails::Application
-  config.generators.javascript_engine = :js
-  config.generators.test_framework = :rspec
-  config.load_defaults 5.1
+module App
+  class Application < Rails::Application
+    config.generators.javascript_engine = :js
+    config.generators.test_framework = :rspec
+    config.load_defaults 5.1
+  end
 end
