1d6efb520a625a563cb302de4e0d9b9c88eb3d6a
Patrick Brown <patrick.arthur.brown@gmail.com>
2018-11-30 17:39:57 -0800

    Ok, this is the last commit tonight i promise

--- app/presenters/user_presenter.rb
+++ app/presenters/user_presenter.rb
@@ -1,15 +1,21 @@
 class UserPresenter < ApplicationPresenter
-  attr_reader :user
-
-  delegate :active_for_authentication?, to: :user
-
-  def initialize(user)
-    @user = user
-  end
+  delegate :active_for_authentication?, :articles, :comments, :karma, to: :model
 
   def username
     return 'deleted' unless active_for_authentication?
 
-    @user.username
+    @model.username
+  end
+
+  def status
+    return 'User account deactivated' unless active_for_authentication?
+
+    'Active user'
+  end
+
+  def priviledges
+    return 'Moderator' if @model.moderator
+
+    'User'
   end
 end
--- spec/presenters/user_presenter_spec.rb
+++ spec/presenters/user_presenter_spec.rb
@@ -24,4 +24,46 @@ RSpec.describe UserPresenter do
       end
     end
   end
+
+  describe '#status' do
+    context 'when user is active' do
+      let(:user) do
+        FactoryBot.build(:user).presenter
+      end
+
+      it 'returns "Active user"' do
+        expect(user.status).to eq('Active user')
+      end
+    end
+
+    context 'when user is deleted' do
+      let(:user) do
+        user = FactoryBot.build(:user)
+        user.soft_delete
+        user.presenter
+      end
+
+      it 'returns "User account deactivated"' do
+        expect(user.status).to eq('User account deactivated')
+      end
+    end
+  end
+
+  describe '#priviledges' do
+    context 'when user is a moderator' do
+      let(:user) { FactoryBot.build(:user, :moderator).presenter }
+
+      it 'returns "Moderator"' do
+        expect(user.priviledges).to eq('Moderator')
+      end
+    end
+
+    context 'when user is not a moderator' do
+      let(:user) { FactoryBot.build(:user).presenter }
+
+      it 'returns "User"' do
+        expect(user.priviledges).to eq('User')
+      end
+    end
+  end
 end
