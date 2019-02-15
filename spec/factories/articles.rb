9a2cac14a2e6a50f2e0b538c06697d586f859f24
Patrick Brown <patrick.arthur.brown@gmail.com>
2019-02-15 05:49:15 -0800

    Fix factories

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :article do
+    sequence(:uid) { SecureRandom.hex(3).to_s }
     sequence(:url) { |e| "https://xkcd.com/#{e}/" }
     sequence(:title) { |e| "Hertzsprung-Russell Diagram #{e}" }
     description { 'The Hertzsprung-Russell diagram' }
