7b9364456cd07e6ad683e0a02eb0a3dfc7a30ff4
Patrick Brown <patrick.arthur.brown@gmail.com>
2019-02-15 06:45:50 -0800

    Perform more actions in docker

--- docker_push.sh
+++ docker_push.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-docker-compose build quasars nginx
-
 docker tag kineticdial/quasars:latest kineticdial/quasars:$(git rev-parse HEAD)
 docker tag kineticdial/quasars-nginx:latest kineticdial/quasars-nginx:$(git rev-parse HEAD)
 
